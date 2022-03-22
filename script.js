5a78335def79457e17209dfb9e7784d905ad0d27
Milos Milijasevic <milosrm20005@gmail.com>
2022-03-22 20:43:53 +0100

    updated

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 "use strict";
 
+const result = document.querySelector(`.result`);
+const preview = document.querySelector(`.preview`);
+
+const buttonDeleteAll = document.querySelector(`.delete-all`);
+const buttonBackSpace = document.querySelector(`.backspace`);
+
+const buttonEqual = document.querySelector(`.equal`);
+const buttonDevide = document.querySelector(`.devide`);
+const buttonPlus = document.querySelector(`.plus`);
+const buttonMinus = document.querySelector(`.minus`);
+const buttonTimes = document.querySelector(`.times`);
+const buttonDot = document.querySelector(`.dot`);
+
+const buttonZero = document.querySelector(`.zero`);
+const buttonOne = document.querySelector(`.one`);
+const buttonTwo = document.querySelector(`.two`);
+const buttonThree = document.querySelector(`.three`);
+const buttonFour = document.querySelector(`.four`);
+const buttonFive = document.querySelector(`.five`);
+const buttonSix = document.querySelector(`.six`);
+const buttonSeven = document.querySelector(`.seven`);
+const buttonEight = document.querySelector(`.eight`);
+const buttonNine = document.querySelector(`.nine`);
